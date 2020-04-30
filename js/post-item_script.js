3f09d22e3b9ff6dfd6292d658f43b367b2a2fcdc
William He <contact@williamhe.dev>
2020-04-30 00:54:12 -0700

    Added Firestore functionality. Some bugs exist

--- js/post-item_script.js
+++ js/post-item_script.js
@@ -1,6 +1,32 @@
+//--------------------------------------//
+//                                      //
+// Constants                            //
+//                                      //
+//--------------------------------------//
+
+// Item categories.
 const itemCategories = ["Select a category...", "Toiletries", "Femenine hygeine", "Cleaning supplies"];
+// Service categories.
 const serviceCategories = ["Select a category...", "Renovation", "Plumbing", "Electrical"];
 
+//--------------------------------------//
+//                                      //
+// Global Variables                     //
+//                                      //
+//--------------------------------------//
+
+// The user currently signed in.
+let thisUser = "test";
+
+//--------------------------------------//
+//                                      //
+// Functions                            //
+//                                      //
+//--------------------------------------//
+
+/**
+ * Shows and hides the quantity menu depending on the category type.
+ */
 function updateQtyDisplay() {
     let radioService = document.getElementById("donationType-1");
     let selectQty = document.getElementById("selectQty-container");
@@ -12,6 +38,9 @@ function updateQtyDisplay() {
     }
 }
 
+/**
+ * Shows text input for quantity of "More..." is selected from the quantity select box.
+ */
 function updateTextQtyDisplay() {
     let selectQty = document.getElementById("selectQty");
     let textQty = document.getElementById("textQty-container");
@@ -23,6 +52,9 @@ function updateTextQtyDisplay() {
     }
 }
 
+/**
+ * Updates the category options depending on the category type.
+ */
 function updateCategoryOptions() {
     let donationType = document.getElementById("donationType-0");
     let selectCategory = document.getElementById("selectCategory");
@@ -63,8 +95,91 @@ function updateCategoryOptions() {
             selectCategory.appendChild(option);
         }
     }
-<<<<<<< Updated upstream
 }
-=======
+
+/**
+ * Submits the post.
+ */
+function submitPost() {
+    let post = getValues();
+
+    let itemRef = db.collection("Items").add(post);
 }
->>>>>>> Stashed changes
+
+/**
+ * Gets the values from the fieldset.
+ */
+function getValues() {
+    // DOM elements
+    let postTypeDOM = document.getElementById("postType-0");
+    let donationTypeDOM = document.getElementById("donationType-0");
+    let itemNameDOM = document.getElementById("itemName");
+    let itemDescDOM = document.getElementById("itemDescription");
+    let itemQtyDOM = document.getElementById("selectQty");
+    let commentsDOM = document.getElementById("taComments");
+    let itemCatDOM = document.getElementById("selectCategory");;
+    let itemImgDOM = document.getElementById("postImage");
+    // Values
+    let postType;
+    let donationType;
+    let itemName;
+    let itemDesc;
+    let itemQty;
+    let comments;
+    let itemCat;
+    let itemImg;
+
+    // Post type
+    if (postTypeDOM.checked) {
+        postType = "donate";
+    } else {
+        postType = "receive";
+    }
+    // Donation type
+    if (donationTypeDOM.checked) {
+        donationType = "item";
+    } else {
+        donationType = "service";
+    }
+    // Item name
+    itemName = itemNameDOM.value;
+    // Item description
+    itemDesc = itemDescDOM.value;
+    // Item quantity
+    if (itemQtyDOM.value != "more") {
+        itemQty = itemQtyDOM.value;
+    } else {
+        itemQtyDOM = document.getElementById("textQty");
+        itemQty = itemQtyDOM.value;
+    }
+    // Comments
+    comments = commentsDOM.value;
+    // Item category
+    itemCat = itemCatDOM.value;
+    // Item image
+    itemImg = "imageFile";
+    //itemImg = itemImgDOM.files;
+
+    return {thisUser, postType, donationType, itemName, itemDesc, itemQty, comments, itemCat, itemImg};
+}
+
+/**
+ * Retrieves and sets the current user.
+ */
+function getUser() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            thisUser = user;
+        } else {
+            alert("Not signed in!");
+        }
+    });
+}
+
+//--------------------------------------//
+//                                      //
+// Method Calls                         //
+//                                      //
+//--------------------------------------//
+
+//getUser();
