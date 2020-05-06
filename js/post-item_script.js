d8a11f4533ba9d215af0b338bbcf14c44f337ec8
Ricky Sun <rickysun8008@gmail.com>
2020-05-06 15:19:13 -0700

    ignore this

--- js/post-item_script.js
+++ js/post-item_script.js
@@ -1,36 +1,180 @@
-let postsRef = db.collection("Posts");
+//--------------------------------------//
+//                                      //
+// Constants                            //
+//                                      //
+//--------------------------------------//
 
-//======================//
-// Functions            //
-//======================//
+// Item categories.
+const itemCategories = ["Select a category...", "Toiletries", "Femenine hygeine", "Cleaning supplies"];
+// Service categories.
+const serviceCategories = ["Select a category...", "Renovation", "Plumbing", "Electrical"];
 
-// Returns vaule of selected radio button
-function getRadioValue() {
-    let radios = document.getElementsByName('postType');
+//--------------------------------------//
+//                                      //
+// Global Variables                     //
+//                                      //
+//--------------------------------------//
 
-    for (let i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            return radios[i].value;
+// The user currently signed in.
+let thisUser = "test";
+
+//--------------------------------------//
+//                                      //
+// Functions                            //
+//                                      //
+//--------------------------------------//
+
+/**
+ * Shows and hides the quantity menu depending on the category type.
+ */
+function updateQtyDisplay() {
+    let radioService = document.getElementById("donationType-1");
+    let selectQty = document.getElementById("selectQty-container");
+
+    if (radioService.checked) {
+        selectQty.style.display = "none";
+    } else {
+        selectQty.style.display = "block";
+    }
+}
+
+/**
+ * Shows text input for quantity of "More..." is selected from the quantity select box.
+ */
+function updateTextQtyDisplay() {
+    let selectQty = document.getElementById("selectQty");
+    let textQty = document.getElementById("textQty-container");
+    
+    if (selectQty.value == "more") {
+        textQty.style.display = "block";
+    } else {
+        textQty.style.display = "none";
+    }
+}
+
+/**
+ * Updates the category options depending on the category type.
+ */
+function updateCategoryOptions() {
+    let donationType = document.getElementById("donationType-0");
+    let selectCategory = document.getElementById("selectCategory");
+    let option;
+
+    selectCategory.innerHTML = "";
+
+    if (donationType.checked) {
+        for (let i = 0; i < itemCategories.length; i++) {
+            option = document.createElement("option");
+
+            if (i == 0) {
+                option.value = "";
+                option.disabled = "";
+                option.selected = ""; 
+            } else {
+                option.value = itemCategories[i];
+            }
+
+            option.text = itemCategories[i];
+
+            selectCategory.appendChild(option);
+        }
+    } else {
+        for (let i = 0; i < itemCategories.length; i++) {
+            option = document.createElement("option");
+
+            if (i == 0) {
+                option.value = "";
+                option.disabled = "";
+                option.selected = ""; 
+            } else {
+                option.value = serviceCategories[i];
+            }
+
+            option.text = serviceCategories[i];
+
+            selectCategory.appendChild(option);
         }
     }
 }
 
-function handleClick() {
-    let postType = getRadioValue();
-    let title = document.getElementById("title").value;
-    let description = document.getElementById("itemDescription").value;
-    let imageString = document.getElementById("postImage").value;
-
-    let obj = {};
-    obj["type"] = postType;
-    obj["title"] = title;
-    obj["postDesc"] = description;
-    obj["image"] = imageString;
-    postsRef.add(obj).then(function (docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function (error) {
-            console.error("Error adding document: ", error);
-        });
-
+/**
+ * Submits the post.
+ */
+function submitPost() {
+    let post = getValues();
+    console.log(post);
+    db.collection("Posts").add(post).then(function() {
+        alert();
+        return true;
+    });
+    return false;
 }
+
+/**
+ * Gets the values from the fieldset.
+ */
+function getValues() {
+    // DOM elements
+    let postTypeDOM = document.getElementById("postType-0");
+    let donationTypeDOM = document.getElementById("donationType-0");
+    let postTitleDOM = document.getElementById("postTitle");
+    let postDescDOM = document.getElementById("postDesc");
+    //let itemImgDOM = document.getElementById("postImage");
+
+    // Values
+    let postType;
+    let donationType;
+    let postTitle;
+    let postDesc;
+    let itemImg;
+
+    // Post type
+    if (postTypeDOM.checked) {
+        postType = "OFFERING";
+    } else {
+        postType = "REQEUSTING";
+    }
+    // Donation type
+    if (donationTypeDOM.checked) {
+        donationType = "item";
+    } else {
+        donationType = "service";
+    }
+    // Post title
+    postTitle = postTitleDOM.value;
+
+    // Post description
+    postDesc = postDescDOM.value;
+
+    // Date posted
+    let today = new Date();
+    submissionDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();    
+
+    // Item image
+    itemImg = "imageFile";
+    // itemImg = itemImgDOM.files;
+    // TODO: convert image to base64 string
+
+    return {thisUser, postType, donationType, postTitle, postDesc, submissionDate, itemImg};
+}
+
+/**
+ * Retrieves and sets the current user.
+ */
+function getUser() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            thisUser = user;
+        } else {
+            alert("Not signed in!");
+        }
+    });
+}
+
+//--------------------------------------//
+//                                      //
+// Method Calls                         //
+//                                      //
+//--------------------------------------//
+
+//getUser();
