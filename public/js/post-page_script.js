9c5f0dead810be62f2023090e7d5bcca5677a35b
Ricky Sun <rickysun8008@gmail.com>
2020-05-14 13:21:06 -0700

    user can now delete and save posts

--- public/js/post-page_script.js
+++ public/js/post-page_script.js
@@ -2,6 +2,7 @@
 // Global Variables     //
 //======================//
 let dbRef = db.collection("Posts");
+let userRef = db.collection("Users");
 var storage = firebase.storage();
 var storageRef = storage.ref();
 let postId = window.localStorage.getItem('postId');
@@ -24,31 +25,33 @@ function loadPage() {
     let userAdDOM = document.getElementById("userAd"); // Post creator's address.
     let userPostDOM = document.getElementById("userPost"); // Post creator's postal code.
     let userIdDOM = document.getElementById("userId"); // Post creator's user ID.
-                                                       // This DOM element is purposely hidden.
+    let savePostDOM = document.getElementById("save");
+    // This DOM element is purposely hidden.
 
     // Sets the DOM element's values/attributes
     dbRef.doc(postId).get()
-    .then(function (snap) {
-        let userId = snap.data().thisUserId;
+        .then(function (snap) {
+            let userId = snap.data().thisUserId;
 
-        // Gets the poster's name by their ID.
-        db.collection("Users").doc(userId).get()
-        .then(function(snap) {
-            userNameDOM.innerHTML = snap.data().name;
+            // Gets the poster's name by their ID.
+            db.collection("Users").doc(userId).get()
+                .then(function (snap) {
+                    userNameDOM.innerHTML = snap.data().name;
+                });
+
+            postTitleDOM.innerHTML = snap.data().postTitle;
+            storageRef.child('postImage/' + snap.data().itemImgName).getDownloadURL()
+                .then(function (url) {
+                    imageSectionDOM.src = url;
+                });
+            postDescDOM.innerHTML = snap.data().postDesc;
+            userAdDOM.innerHTML = "<db-value>";
+            userPostDOM.innerHTML = "<db-value>";
+            userIdDOM.value = userId;
+            savePostDOM.onclick = savePost;
+
+            isPosterOwner();
         });
-
-        postTitleDOM.innerHTML = snap.data().postTitle;
-        storageRef.child('postImage/' + snap.data().itemImgName).getDownloadURL()
-        .then(function (url) {
-            imageSectionDOM.src = url;
-        });
-        postDescDOM.innerHTML = snap.data().postDesc;
-        userAdDOM.innerHTML = "<db-value>";
-        userPostDOM.innerHTML = "<db-value>";
-        userIdDOM.value = userId;
-
-        isPosterOwner();
-    });
 }
 
 /**
@@ -61,11 +64,39 @@ function isPosterOwner() {
         if (user.uid == postCreatorId) {
             let deleteButton = document.createElement("button");
             deleteButton.innerHTML = "Delete Post";
+            deleteButton.onclick = deletePost;
             // generate delete post
             document.getElementById("postDiv").appendChild(deleteButton);
         }
     })
 }
+
+/**
+ * Saves the postID under the user's "SavedPosts" collection.
+ */
+function savePost() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        userRef.doc(user.uid).collection("SavedPosts").doc(postId).set({
+                postId: postId
+        })
+            .then(function () {
+                alert("Post successfully saved!");
+            })
+    })
+};
+
+/**
+ * Removes the post from the db entirely.
+ */
+function deletePost() {
+    dbRef.doc(postId).delete()
+        .then(function () {
+            alert("Post successfully deleted!");
+            window.location.href = "main.html";
+        })
+};
+// TODO: if post is deleted, the post must also be deleted from all saved posts
+
 
 //==================================//
 //                                  //
