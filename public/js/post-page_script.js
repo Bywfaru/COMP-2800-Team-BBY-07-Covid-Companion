a4e40bba9500b7dd10e08ce7e7b0e95a27239feb
William He <contact@williamhe.dev>
2020-05-13 20:35:51 -0700

    Created JS file for the page with code which will set the page's values

---
+++ public/js/post-page_script.js
@@ -0,0 +1,37 @@
+//==================================//
+//                                  //
+// Functions                        //
+//                                  //
+//==================================//
+
+/**
+ * Sets the page values with the db's. Should be called after document loads.
+ */
+function loadPage() {
+    // DOM Elements.
+    let postTitleDOM = document.getElementById("postTitle"); // Post's title.
+    let userNameDOM = document.getElementById("userName"); // Post creator's name.
+    let imageSectionDOM = document.getElementById("imageSection"); // Post's image.
+    let postDescDOM = document.getElementById("postDesc"); // Post's description.
+    let userAdDOM = document.getElementById("userAd"); // Post creator's address.
+    let userPostDOM = document.getElementById("userPost"); // Post creator's postal code.
+
+    // Sets the DOM element's values/attributes.
+    // TODO: Get the post's information and change the values to the db's values.
+    //       Replace all "<db-value>" with the db's value. 
+    postTitleDOM.innerHTML = "<db-value>"; 
+    userNameDOM.innerHTML = "<db-value>";
+    imageSectionDOM.src = "<db-value>";
+    postDescDOM.innerHTML = "<db-value>";
+    userAdDOM = "<db-value>";
+    userPostDOM = "<db-value>";
+}
+
+//==================================//
+//                                  //
+// Function Calls                   //
+//                                  //
+//==================================//
+
+// TODO: Uncomment the line of code below once the function has been updated.
+// loadPage();
