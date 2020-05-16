379951072d8fab42821baa440722150a45df3f74
William He <contact@williamhe.dev>
2020-05-15 23:18:46 -0700

    Live chat works I think

--- public/js/chat_script.js
+++ public/js/chat_script.js
@@ -100,80 +100,84 @@ function loadActiveMessage(chatId) {
         .onSnapshot(function(doc) {
             let msgHistoryDiv = document.getElementById("msg_history");
                 msgHistoryDiv.innerHTML = ""; // Clears the div so there are no dupes after each snapshot.
+            
+            if (doc.data()) {
+                // If messages exist in the chat.
+                msgIdArray = doc.data().messageIdArray; // Array containing the message IDs.
 
-            msgIdArray = doc.data().messageIdArray; // Array containing the message IDs.
+                console.log(msgIdArray);
 
-            console.log(msgIdArray);
-
-            msgIdArray
-                .forEach(function(msg) {
-                    console.log(msg);
-                    // Get the message's document reference snapshot.
-                    db.collection("Chats").doc(chatId).collection("Messages").doc(msg)
-                        .onSnapshot(function(messageDoc) {
-                            if (messageDoc.data().from == thisUser.uid) {
-                                // If the message is outgoing.
-                                // Create the DOM elements.
-                                let outgoingMsgDiv = document.createElement("div");
-                                    outgoingMsgDiv.setAttribute("class", "outgoing_msg");
-                                let sentMsgDiv = document.createElement("div");
-                                    sentMsgDiv.setAttribute("class", "sent_msg");
-                                let messageP = document.createElement("p");
-                                let dateSpan = document.createElement("span")
-                                    dateSpan.setAttribute("class", "time_date");
-                            
-                                // Set the attributes and inner HTML.
-                                messageP.innerHTML = messageDoc.data().message;
-                                // TODO: Set the date and time to the date and time in the db.
-                                dateSpan.innerHTML = messageDoc.data().time;
-                            
-                                // Append the DOM elements.
-                                sentMsgDiv.appendChild(messageP);
-                                sentMsgDiv.appendChild(dateSpan);
-                            
-                                outgoingMsgDiv.appendChild(sentMsgDiv);
-                            
-                                msgHistoryDiv.appendChild(outgoingMsgDiv);
-                            } else {
-                                // If the message is incoming.
-                                // Create the DOM elements.
-                                let msgHistoryDiv = document.getElementById("msg_history");
+                msgIdArray
+                    .forEach(function(msg) {
+                        console.log(msg);
+                        // Get the message's document reference snapshot.
+                        db.collection("Chats").doc(chatId).collection("Messages").doc(msg)
+                            .onSnapshot(function(messageDoc) {
+                                if (messageDoc.data().from == thisUser.uid) {
+                                    // If the message is outgoing.
+                                    // Create the DOM elements.
+                                    let outgoingMsgDiv = document.createElement("div");
+                                        outgoingMsgDiv.setAttribute("class", "outgoing_msg");
+                                    let sentMsgDiv = document.createElement("div");
+                                        sentMsgDiv.setAttribute("class", "sent_msg");
+                                    let messageP = document.createElement("p");
+                                    let dateSpan = document.createElement("span")
+                                        dateSpan.setAttribute("class", "time_date");
                                 
-                                let incomingMsgDiv = document.createElement("div");
-                                    incomingMsgDiv.setAttribute("class", "incoming_msg");
-                                let profilePicImg = document.createElement("img");
-                                    profilePicImg.setAttribute("alt", "profle-pic");
-                                let incomingMsgImgDiv = document.createElement("div");
-                                    incomingMsgImgDiv.setAttribute("class", "incoming_msg_img");
-                                let receivedMsgDiv = document.createElement("div");
-                                    receivedMsgDiv.setAttribute("class", "received_msg");
-                                let receivedWithdMsgDiv = document.createElement("div");
-                                    receivedWithdMsgDiv.setAttribute("class", "received_withd_msg");
-                                let messageP = document.createElement("p");
-                                let dateSpan = document.createElement("span")
-                                    dateSpan.setAttribute("class", "time_date");
-                            
-                                // Set the attributes and inner HTML.
-                                profilePicImg.src = "https://randomuser.me/api/portraits/women/59.jpg";
-                                messageP.innerHTML = messageDoc.data().message;
-                                // TODO: Set the date and time to the date and time in the db.
-                                dateSpan.innerHTML = messageDoc.data().time;
-                            
-                                // Append the DOM elements.
-                                incomingMsgImgDiv.appendChild(profilePicImg);
-                            
-                                receivedWithdMsgDiv.appendChild(messageP);
-                                receivedWithdMsgDiv.appendChild(dateSpan);
-                            
-                                receivedMsgDiv.appendChild(receivedWithdMsgDiv);
-                            
-                                incomingMsgDiv.appendChild(incomingMsgImgDiv);
-                                incomingMsgDiv.appendChild(receivedMsgDiv);
-                            
-                                msgHistoryDiv.appendChild(incomingMsgDiv);
-                            }
-                        });
-                });
+                                    // Set the attributes and inner HTML.
+                                    messageP.innerHTML = messageDoc.data().message;
+                                    // TODO: Set the date and time to the date and time in the db.
+                                    dateSpan.innerHTML = messageDoc.data().time;
+                                
+                                    // Append the DOM elements.
+                                    sentMsgDiv.appendChild(messageP);
+                                    sentMsgDiv.appendChild(dateSpan);
+                                
+                                    outgoingMsgDiv.appendChild(sentMsgDiv);
+                                
+                                    msgHistoryDiv.appendChild(outgoingMsgDiv);
+                                } else {
+                                    // If the message is incoming.
+                                    // Create the DOM elements.
+                                    let msgHistoryDiv = document.getElementById("msg_history");
+                                    
+                                    let incomingMsgDiv = document.createElement("div");
+                                        incomingMsgDiv.setAttribute("class", "incoming_msg");
+                                    let profilePicImg = document.createElement("img");
+                                        profilePicImg.setAttribute("alt", "profle-pic");
+                                    let incomingMsgImgDiv = document.createElement("div");
+                                        incomingMsgImgDiv.setAttribute("class", "incoming_msg_img");
+                                    let receivedMsgDiv = document.createElement("div");
+                                        receivedMsgDiv.setAttribute("class", "received_msg");
+                                    let receivedWithdMsgDiv = document.createElement("div");
+                                        receivedWithdMsgDiv.setAttribute("class", "received_withd_msg");
+                                    let messageP = document.createElement("p");
+                                    let dateSpan = document.createElement("span")
+                                        dateSpan.setAttribute("class", "time_date");
+                                
+                                    // Set the attributes and inner HTML.
+                                    profilePicImg.src = "https://randomuser.me/api/portraits/women/59.jpg";
+                                    messageP.innerHTML = messageDoc.data().message;
+                                    // TODO: Set the date and time to the date and time in the db.
+                                    dateSpan.innerHTML = messageDoc.data().time;
+                                
+                                    // Append the DOM elements.
+                                    incomingMsgImgDiv.appendChild(profilePicImg);
+                                
+                                    receivedWithdMsgDiv.appendChild(messageP);
+                                    receivedWithdMsgDiv.appendChild(dateSpan);
+                                
+                                    receivedMsgDiv.appendChild(receivedWithdMsgDiv);
+                                
+                                    incomingMsgDiv.appendChild(incomingMsgImgDiv);
+                                    incomingMsgDiv.appendChild(receivedMsgDiv);
+                                
+                                    msgHistoryDiv.appendChild(incomingMsgDiv);
+                                }
+                            });
+                    });
+            }
+            
         });
 }
 
@@ -183,10 +187,9 @@ function loadActiveMessage(chatId) {
 function sendMessage() {
     let message = document.getElementById("write_msg");
     let chatId = localStorage.getItem("activeChatId"); // The chat ID to send to.
-    let messageId; // The message's unique ID.
     let messageDocRef = db.collection("Chats").doc(chatId).collection("Messages").doc();
     let chatDocRef = db.collection("Chats").doc(chatId);
-
+    console.log("test");
     messageDocRef
         .set({
             // Sets the message's properties.
@@ -199,6 +202,12 @@ function sendMessage() {
                 .update({
                     // Adds the new message's ID to the array.
                     messageIdArray: firebase.firestore.FieldValue.arrayUnion(messageDocRef.id)
+                })
+                .catch(function() {
+                    chatDocRef
+                        .set({
+                            messageIdArray: [messageDocRef.id]
+                        });
                 });
         });
 }
