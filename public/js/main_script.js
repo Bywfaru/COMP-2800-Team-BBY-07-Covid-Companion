4391a52bc5d3a02175cf66a9a7a8e1d1ad98637a
Kashish Bansal <aashma.2008@gmail.com>
2020-05-19 13:32:04 -0400

    styling main feed

--- public/js/main_script.js
+++ public/js/main_script.js
@@ -161,3 +161,23 @@ function createOneCard(c) {
 // Main()               //
 //======================//
 getPosts();
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "75%";
+  }
+}
