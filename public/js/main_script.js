ba37b226f2122a20548333ea09b304bf5825c0a6
Ricky Sun <rickysun8008@gmail.com>
2020-05-20 16:06:50 -0700

    removed jumbotron, started grid layout...

--- public/js/main_script.js
+++ public/js/main_script.js
@@ -81,81 +81,8 @@ function displayCards(CardObjects) { //takes in collection
     })
 };
 
-/**
- * Creates a post.
- * 
- * @param c the post document
- */
-function createOneCard(c) {
-    var coldiv = document.createElement("div");
-    coldiv.setAttribute("class", "col-md-auto");
+//TODO: create dynamic grid with cards
 
-    var carddiv = document.createElement("div");
-    carddiv.setAttribute("class", "card");
-
-    var cardbodydiv = document.createElement("div");
-    cardbodydiv.setAttribute("class", "card-body");
-
-    // Post TYPE
-    var type = document.createElement("h4");
-    type.setAttribute("class", "card-title");
-    var text = document.createTextNode(c.data().postType + ":");
-    type.appendChild(text);
-
-    // Post TITLE
-    var title = document.createElement("h4");
-    title.setAttribute("class", "card-text");
-    var text = document.createTextNode(c.data().postTitle);
-    title.appendChild(text);
-
-    // Post IMAGE
-    var image = document.createElement("img");
-    image.setAttribute("class", "card-img");
-    storageRef.child('postImage/' + c.data().itemImgName).getDownloadURL().then(function (url) {
-        image.src = url;
-    });
-
-    // Post DESCRIPTION
-    var desc = document.createElement("p");
-    desc.setAttribute("class", "card-text");
-    var text = document.createTextNode(c.data().postDesc);
-    desc.appendChild(text);
-
-    // Posted DATE
-    var date = document.createElement("p");
-    date.setAttribute("class", "card-text");
-    let subDate = c.data().submissionDate; // STRING
-    let d = new Date(subDate);
-    let year = d.getFullYear();
-    let month = monthNames[d.getMonth()];
-    let day = d.getDate();
-    var text = document.createTextNode(month + " " + day + ", " + year);
-    date.appendChild(text);
-
-    // View Post button
-    var a = document.createElement("input");
-    a.type = "button"
-    a.setAttribute("value", "View");
-    a.addEventListener('click', function () {
-        window.location.href = "post-page.html";
-        let postId = c.id;
-        window.localStorage.setItem("postId", postId);
-    });
-    a.setAttribute("class", "btn btn-outline-secondary");
-    var text = document.createTextNode("View Gym");
-    a.appendChild(text);
-
-    // Stitch it all together 
-    cardbodydiv.appendChild(type);
-    cardbodydiv.appendChild(title);
-    cardbodydiv.appendChild(image);
-    cardbodydiv.appendChild(desc);
-    cardbodydiv.appendChild(date);
-    cardbodydiv.appendChild(a);
-    carddiv.appendChild(cardbodydiv);
-    coldiv.appendChild(carddiv);
-    document.getElementById("cards").appendChild(coldiv); //stick it in the div
-}
 
 //======================//
 // Main()               //
